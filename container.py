96e574fe7aa953daaa66259ebdeabf29c296a56e
greerji <greerji@mail.uc.edu>
2016-06-01 10:48:18 -0400

    Added container.py (Stack and Queue)

---
+++ container.py
@@ -0,0 +1,55 @@
+class Stack(object):
+    def __init__(self):
+        self.list = []
+        self.size = 0
+    def push(self, val):
+        self.size += 1
+        self.list.append(val)
+    def pop(self):
+        if self.size > 0:
+            self.size -= 1
+            top = self.list[-1]
+            self.list = self.list[:-1]
+            return top
+    def top(self):
+        return self.list[-1]
+
+class Queue(object):
+    def __init__(self):
+        self.list = []
+        self.size = 0
+    def enq(self, val):
+        self.size += 1
+        self.list.append(val)
+    def deq(self):
+        if self.size > 0:
+            self.size -= 1
+            k = self.list[0]
+            self.list = self.list[1:]
+            return k
+    def front(self):
+        return self.list[0]
+
+
+stk = Stack()
+stk.push(5)
+stk.push(3)
+stk.push(7)
+
+for x in range(stk.size):
+    print stk.pop()
+
+que = Queue()
+que.enq(2)
+que.enq(4)
+que.enq(7)
+
+for x in range(que.size):
+    print que.deq()
+
+
+
+
+
+
+
